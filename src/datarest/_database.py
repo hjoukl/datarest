8b9c47bc75c6fa760a5c2814e426feab5346377f
hjoukl <holger.joukl@gmx.de>
2023-01-24 18:13:39 +0100

    Use string templating to allow credentials from env (fixes #13) (#14)

--- src/datarest/_database.py
+++ src/datarest/_database.py
@@ -1,13 +1,26 @@
+import os
+import string
+
 from sqlalchemy import create_engine
 from sqlalchemy import orm
 
 from ._app_config import config
 
 
-connect_string = config.datarest.database.connect_string
-# check_same_thread needed for sqlite only
+connect_string = string.Template(
+    config.datarest.database.connect_string
+    ).substitute(os.environ)
+
+# TODO: Make check more robust?
+# Create connect string using pydantic, e.g. pydantic.PostgresDsn?
+if connect_string.startswith('sqlite:'):
+    # check_same_thread needed for sqlite only
+    connect_args = {"check_same_thread": False}
+else:
+    connect_args = {}
+
 engine = create_engine(
-    connect_string, connect_args={"check_same_thread": False})
+    connect_string, connect_args=connect_args)
 
 Session = orm.Session
 SessionLocal = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
